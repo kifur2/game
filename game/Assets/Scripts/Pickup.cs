6455cf901863df829cc9829c8e92c671dc6797b0
kifur2 <91947941+kifur2@users.noreply.github.com>
2024-04-21 20:13:58 +0200

    17 temporary boosts pick up (#23)
    * [17] added some temporary boosts
    
    * [17] added temp boosts icons + fire rate up
    
    ---------
    
    Co-authored-by: Jakub <jakub.cudak@student.pk.edu.pl>

---
+++ game/Assets/Scripts/Pickup.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public enum Type
+    {
+        Diamond,
+        Heart,
+        Shield,
+        Sword,
+        Bow
+    }
+
+    [SerializeField] private float timedEffectDuration = 0;
+    [SerializeField] private Type selectedType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        
+        var playerProperties = other.GetComponent<PlayerProperties>();
+        if (playerProperties == null) return;
+        
+        var uiIconsQueue = FindObjectOfType<UIIconsQueue>();
+        
+        switch (selectedType)
+        {
+            case Type.Diamond:
+                playerProperties.AddDiamond();
+                break;
+            case Type.Heart:
+                playerProperties.Heal();
+                break;
+            case Type.Shield:
+                StartCoroutine(playerProperties.InvincibilityEffect(timedEffectDuration));
+                uiIconsQueue.AddIcon(selectedType, timedEffectDuration);
+                break;
+            case Type.Sword:
+                StartCoroutine(playerProperties.SuperDamageEffect(timedEffectDuration));
+                uiIconsQueue.AddIcon(selectedType, timedEffectDuration);
+                break;
+            case Type.Bow:
+                StartCoroutine(playerProperties.SuperFireRateEffect(timedEffectDuration));
+                uiIconsQueue.AddIcon(selectedType, timedEffectDuration);
+                break;
+            default:
+                Debug.LogWarning("Unhandled pickup type: " + selectedType);
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+}
--- game/Assets/Scripts/PlayerMotor.cs
+++ game/Assets/Scripts/PlayerMotor.cs
@@ -4,85 +4,71 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 velocity;
-    private bool isGrounded;
-    private bool isSprinting;
-    private bool isCrouching;
-    private bool lerpCrouch;
-    private float crouchTimer;
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    private bool _isGrounded;
+    private bool _isSprinting;
+    private bool _isCrouching;
+    private bool _lerpCrouch;
+    private float _crouchTimer;
     public float walkSpeed = 5.0f;
-    private float speed = 5.0f;
+    private float _speed = 5.0f;
     public float sprintSpeed = 8.0f;
     public float jumpHeight = 5.0f;
-    private float gravity = -9.81f;
+    private readonly float _gravity = -9.81f;
     
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = controller.isGrounded;
-        if (lerpCrouch)
-        {
-            crouchTimer += Time.deltaTime;
-            float p = crouchTimer;
-            p*=p;
-            if (isCrouching)
-            {
-                controller.height = Mathf.Lerp(controller.height, 1, p);
-            }
-            else
-            {
-                controller.height = Mathf.Lerp(controller.height, 2, p);
-            }
-            if(p>1)
-            {
-                lerpCrouch = false;
-                crouchTimer = 0;
-            }
-        }
+        _isGrounded = _controller.isGrounded;
+        if (!_lerpCrouch) return;
+        _crouchTimer += Time.deltaTime;
+        var p = _crouchTimer;
+        p*=p;
+        _controller.height = Mathf.Lerp(_controller.height, _isCrouching ? 1 : 2, p);
+        if (!(p > 1)) return;
+        _lerpCrouch = false;
+        _crouchTimer = 0;
     }
     
     public void MovePlayer(Vector2 input)
     {
         Vector3 moveDirection = new Vector3(input.x, 0, input.y);
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
-        velocity.y += gravity * Time.deltaTime;
+        _controller.Move(transform.TransformDirection(moveDirection) * _speed * Time.deltaTime);
+        _velocity.y += _gravity * Time.deltaTime;
         
-        if(isGrounded && velocity.y < 0)
+        if(_isGrounded && _velocity.y < 0)
         {
-            velocity.y = -2f;
+            _velocity.y = -2f;
         }
         
-        controller.Move(velocity * Time.deltaTime);
+        _controller.Move(_velocity * Time.deltaTime);
     }
     
     public void Jump()
     {
-        if(isGrounded)
+        if(_isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -gravity);
+            _velocity.y = Mathf.Sqrt(jumpHeight * -_gravity);
         }
     }
 
     public void Crouch()
     {
-        isCrouching = !isCrouching;
-        crouchTimer = 0;
-        lerpCrouch = true;
+        _isCrouching = !_isCrouching;
+        _crouchTimer = 0;
+        _lerpCrouch = true;
     }
     
     public void Sprint()
     {
-        isSprinting = !isSprinting;
-        if (isSprinting)
-            speed = sprintSpeed;
-        else
-            speed = walkSpeed;
+        _isSprinting = !_isSprinting;
+        _speed = _isSprinting ? sprintSpeed : walkSpeed;
     }
 }
