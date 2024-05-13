ffea01e538ea4b11912596bf9304c85c90fca2cb
Jakub <jakub.cudak@student.pk.edu.pl>
2024-05-13 18:09:46 +0200

    [27] mesh generated

--- game/Assets/Scripts/MapGeneration/MeshGenerator.cs
+++ game/Assets/Scripts/MapGeneration/MeshGenerator.cs
@@ -7,6 +7,7 @@ public static class MeshGenerator
     public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier,
         AnimationCurve meshHeightCurve, int levelOfDetail)
     {
+        AnimationCurve heightCurve = new AnimationCurve(meshHeightCurve.keys);
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
         float topLeftX = (width - 1) / -2f;
@@ -23,7 +24,7 @@ public static class MeshGenerator
             for (int x = 0; x < width; x += meshSimplificationIncrement)
             {
                 meshData.vertices[vertexIndex] = new Vector3(topLeftX + x,
-                    meshHeightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
+                    heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
                 meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
 
                 if (x < width - 1 && y < height - 1)
